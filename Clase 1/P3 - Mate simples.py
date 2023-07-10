a15b86295c8358ac04578078921d904d544fa2fb
ricardomonlpz <ricardomonroy58@gmail.com>
2023-07-09 18:18:50 -0600

    up

---
+++ Clase 1/P3 - Mate simples.py
@@ -0,0 +1,29 @@
+#Ejercicio 1: Promedio
+
+Literatura=9.1
+Algebra_Lineal=9.0
+Estadistica=8.5
+Progra_Compu=10
+Ingles=8.0
+
+print("\nRespuesta Ejercicio 1")
+print("Tu promedio es: ", (Literatura+Algebra_Lineal+Estadistica+Progra_Compu+Ingles)/5)
+
+#Ejercicio 2 Conversión de Grados Celsius Farhenheit
+
+Celsius = 33
+
+print("\nRespuesta Ejercicio 2")
+print("Grados Celsius:",Celsius,", Grados Farhenheir:",(Celsius*(9/5)+32))
+
+#Ejercicio 3 Obtener longitud de lados de un triángulo rectángulo
+
+from math import sqrt
+
+Cateto1=50
+Cateto2=100
+Hipotenusa=sqrt(Cateto1**2+Cateto2**2)
+
+print("\nRespuesta Ejercicio 3")
+print("Gateto 1:",Cateto1,", Cateto 2:",Cateto2,"Hipotenusa",Hipotenusa,"\n")
+
