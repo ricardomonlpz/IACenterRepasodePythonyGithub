8422cde1ffc4482cb2d5705c089a96ddb89f854d
ricardomonlpz <ricardomonroy58@gmail.com>
2023-07-18 13:05:54 -0600

    up

---
+++ Clase 1/P4 -Entradas y salidas simples.py
@@ -0,0 +1,9 @@
+a = float(input('ingresa el balor de a: '))
+b = float(input('ingresa el valor de b: '))
+
+print('\nLa suma de \t\t',a,'+',b,'=',a+b)
+print('La resta de \t\t',a,'-',b,'=',a-b)
+print('La multiplicación de \t',a,'*',b,'=',a*b)
+print('La divición de \t\t',a,'/',b,'=',a/b)
+
+print("\n¡Eso es todo, amigos!")
