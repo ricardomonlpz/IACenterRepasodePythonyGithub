a10bba10da7bea52fc0632a0bf70150447742680
ricardomonlpz <ricardomonroy58@gmail.com>
2023-07-25 11:40:46 -0600

    Ricardo Monroy

--- Clase 1/P7 -Maximo de 4 numeros.py
+++ Clase 1/P7 -Maximo de 4 numeros.py
@@ -3,11 +3,15 @@ numero2 = int(input("Ingresa el segundo número: "))
 numero3 = int(input("Ingresa el tercer número: "))
 numero4 = int(input("Ingresa el cuarto número: "))
 
-if numero1 >= numero2 and numero1 >= numero3 and numero1 >= numero4:
-    print("El numero",numero1,"es el numero mas grande")
-elif numero2 >= numero1 and numero2 >= numero3 and numero2 >= numero4:
-    print("El numero",numero2,"es el numero mas grande")
-elif numero3 >= numero1 and numero3 >= numero2 and numero3 >= numero4:
-    print("El numero",numero3,"es el numero mas grande")
+if numero1 >= numero2:
+    nummax=numero1
 else:
-    print("El numero",numero4,"es el numero mas grande")
+    nummax=numero2
+
+if numero3 >= nummax:
+    nummax=numero3
+
+if numero4 >= nummax:
+    nummax=numero4
+
+print("El numero más grande es:",nummax)
